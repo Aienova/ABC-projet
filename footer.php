f96972c32f92ec8ff12fca42cd4511cdb8b3c821
mayd-diallo <maydiallo78@gmail.com>
2025-08-04 16:51:28 +0200

    foter mamadou

--- footer.php
+++ footer.php
@@ -1,5 +1,23 @@
 
-    <footer class="centralizer">Copyright © 2025 ABC Dev Centre de Réadaptation de Mulhouse</footer>
+    <footer>
+        <p class="centralizer">Copyright © 2025 ABC Dev Centre de Réadaptation de Mulhouse</p>
+
+     <div class="social">
+
+       <a href="https://github.com/Aienova/ABC-projet" ><img src="<?php echo $root; ?>/media/x.png"/></a>
+         <a href="https://github.com/Aienova/ABC-projet" ><img src="<?php echo $root; ?>/media/facebook.png"/></a>  
+            <a href="https://github.com/Aienova/ABC-projet" ><img src="<?php echo $root; ?>/media/instagram.png"/></a>
+        
+    </div>
+    </footer>
+
+
+ 
+   
+
+
+
+
 
 </body>
 </html>
