58029fa3ada2e720c8b296f5da4f6b451b52e423
mayd-diallo <maydiallo78@gmail.com>
2025-08-05 14:16:56 +0200

    footerv1

--- footer.php
+++ footer.php
@@ -1,14 +1,33 @@
 
     <footer>
-        <p class="centralizer">Copyright © 2025 ABC Dev Centre de Réadaptation de Mulhouse</p>
+
+
+    <ul>
+      <li><a href="<?php echo $root; ?>/">Mentions légales</a></li>
+      <li><a href="<?php echo $root; ?>/">|</a></li>
+            <li><a href="<?php echo $root; ?>/">Politique de confidentialité</a></li>
+      <li><a href="<?php echo $root; ?>/">|</a></li>
+      <li><a href="<?php echo $root; ?>/"> F.A.Q</a></li>
+      
+      
+</ul>
+<ul>
+      <li><a href="<?php echo $root; ?>/">Nous</a></li>
+            <li><a href="<?php echo $root; ?>/">suivre</a></li>
+</ul>
+    
 
      <div class="social">
 
-       <a href="https://github.com/Aienova/ABC-projet" ><img src="<?php echo $root; ?>/media/x.png"/></a>
-         <a href="https://github.com/Aienova/ABC-projet" ><img src="<?php echo $root; ?>/media/facebook.png"/></a>  
-            <a href="https://github.com/Aienova/ABC-projet" ><img src="<?php echo $root; ?>/media/instagram.png"/></a>
-        
+       <a href="https://github.com/Aienova/ABC-projet" target="_blank" ><img src="<?php echo $root; ?>/media/x.png"/></a>
+         <a href="https://github.com/Aienova/ABC-projet" target="_blank"><img src="<?php echo $root; ?>/media/facebook.png"/></a>  
+            <a href="https://github.com/Aienova/ABC-projet" target="_blank"><img src="<?php echo $root; ?>/media/instagram.png"/></a>
+      
+
     </div>
+
+    <p class="centralizer">Copyright © 2025 ABC Dev Centre de Réadaptation de Mulhouse</p>
+
     </footer>
 
 
