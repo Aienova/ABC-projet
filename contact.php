d8374a1d3b6effc7cc3877c1e7c097a560ce0343
Kent-cyber <mrkentfire96@gmail.com>
2025-08-05 16:45:07 +0200

    quentin v3

--- contact.php
+++ contact.php
@@ -2,57 +2,11 @@
 $pageTitle = "AIE TV - Contact";
 
 include "header.php" ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        .form-container {
-            background-color: red;
-            color: black;
-            max-width: 700px;
-            margin: auto;
-            padding: 30px;
-        }
 
-        form {
-            display: flex;
-            flex-direction: column;
-        }
-
-        .form-container input {
-            width: 50%;
-            padding: 10px;
-            margin-top: 10px;
-            margin-bottom: 20px;
-            border: none;
-        }
-
-        .form-container textarea {
-            width: 100%;
-            padding: 10px;
-            margin-top: 10px;
-            margin-bottom: 20px;
-            border: none;
-            resize: none;
-        }
-
-        .form-container button {
-            background-color: black;
-            width: 25%;
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
-    </style>
-</head>
-<body>
-    <main>
 
         <section id="services">
             <h2>Contact</h2>
+            <!--FORM-->
 
             <div class="form-container">
                <form method="POST" action="">
@@ -71,8 +25,7 @@ include "header.php" ?>
                 </form>
             </div> 
         </section>
-    </main>
-</body>
+
 
 </html>
 <?php include "footer.php" ?>
